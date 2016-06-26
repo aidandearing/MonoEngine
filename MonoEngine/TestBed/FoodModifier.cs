45c583ee7d42c95f00113e32a30b84ce2389591d
aidandearing <aidan.dearing@live.com>
2016-06-26 19:58:22 -0400

    Testbed of Food
    Tried to figure out how best to implement the backend for modifiable
    food, it turned out quite well, now for Mat to expand it massively.

---
+++ MonoEngine/TestBed/FoodModifier.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBed
+{
+    public class FoodModifier
+    {
+        public enum Modifier { add, multiply, prepend, append };
+
+        public Modifier modifier;
+        public FoodState.CutState cutstate;
+        public FoodState.TempState tempstate;
+        public string value;
+
+        public FoodModifier(Modifier mod, FoodState.CutState cs, FoodState.TempState ts, string val)
+        {
+            modifier = mod;
+            value = val;
+
+            cutstate = cs;
+            tempstate = ts;
+        }
+
+        public int GetValue(int val)
+        {
+            switch(modifier)
+            {
+                case Modifier.add:
+                    return int.Parse(value) + val;
+                case Modifier.multiply:
+                    return int.Parse(value) * val;
+                case Modifier.prepend:
+                    return int.Parse(value + val.ToString());
+                case Modifier.append:
+                    return int.Parse(val.ToString() + value);
+            }
+
+            return val;
+        }
+
+        public float GetValue(float val)
+        {
+            switch (modifier)
+            {
+                case Modifier.add:
+                    return float.Parse(value) + val;
+                case Modifier.multiply:
+                    return float.Parse(value) * val;
+                case Modifier.prepend:
+                    return float.Parse(value + val.ToString());
+                case Modifier.append:
+                    return float.Parse(val.ToString() + value);
+            }
+
+            return val;
+        }
+
+        public string GetValue(string val)
+        {
+            switch (modifier)
+            {
+                case Modifier.add:
+                    return value + val;
+                case Modifier.multiply:
+                    return value + val;
+                case Modifier.prepend:
+                    return value + val;
+                case Modifier.append:
+                    return val + value;
+            }
+
+            return val;
+        }
+    }
+}
---
+++ MonoEngine/TestBed/FoodState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBed
+{
+    public class FoodState
+    {
+        public enum CutState { idc, none, sliced, pasted, blended };
+        public enum TempState { idc, frozen, cooled, room, warm };
+
+        public CutState cutstate;
+        public TempState tempstate;
+        public string name;
+
+        public FoodState(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name()
+        {
+            return Food.foods[name].Name(cutstate, tempstate);
+        }
+
+        public int Stack()
+        {
+            return Food.foods[name].Stack(cutstate, tempstate);
+        }
+
+        public float Value()
+        {
+            return Food.foods[name].Value(cutstate, tempstate);
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("\n" + Name() + "\n" + Stack() + "\n" + Value());
+        }
+    }
+}
