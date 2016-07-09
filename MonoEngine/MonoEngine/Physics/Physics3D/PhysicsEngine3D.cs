37e1bd8d6ccb8722286a0f25abfd6c9d11734353
aidandearing <aidan.dearing@live.com>
2016-07-08 21:06:44 -0400

    PhysicsEngine.PhysicsSettings FIXED Ambiguity in settings name
    Renamed MODEL_SCALE -> MODEL_TRANSLATION_SCALE
    To better represent what the value actually controls, which is not the
    scale of the models, but how much any 1 unit translation actually moves
    the model in the world space, to resolve issues between scale and
    translation not being in the same value.

--- MonoEngine/MonoEngine/Physics/Physics3D/PhysicsEngine3D.cs
+++ MonoEngine/MonoEngine/Physics/Physics3D/PhysicsEngine3D.cs
@@ -42,8 +42,8 @@ namespace MonoEngine.Physics.Physics3D
             //bodies_Dead = new List<PhysicsBody>();
 
             // Jenky as magic numbers, sorry. Works out this way for the scale we decided on in 3DSMax.
-            worldToRender = Matrix.CreateScale(PhysicsSettings.MODEL_SCALE);
-            renderToWorld = Matrix.CreateScale(1.0f / PhysicsSettings.MODEL_SCALE);
+            worldToRender = Matrix.CreateScale(PhysicsSettings.MODEL_TRANSLATION_SCALE);
+            renderToWorld = Matrix.CreateScale(1.0f / PhysicsSettings.MODEL_TRANSLATION_SCALE);
 
             base.Initialize();
         }
