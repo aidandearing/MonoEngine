0536b293685fef769cd3484ad52f72083c445e6a
matbecker <7m4tt3r7@gmail.com>
2016-08-28 15:02:22 -0400

    moved Font to Assets Folder

---
+++ MonoEngine/MonoEngine/Assets/Font.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoEngine.Render
+{
+    public class Font
+    {
+        private static Dictionary<string, Font> fonts = new Dictionary<string, Font>();
+        public int[] fontSizes;
+        public SpriteFont[] fontStyles;
+
+        public Font(int[] fontSizes, SpriteFont[] fontStyles)
+        {
+            this.fontSizes = fontSizes;
+            this.fontStyles = fontStyles;
+
+            fonts = new Dictionary<string, Font>();
+        }
+
+        public SpriteFont GetFont(int size)
+        {
+            for (int i = 0; i < fontSizes.Length; i++)
+            {
+                if (size <= fontSizes[i] || i == fontSizes.Length - 1)
+                {
+                    return fontStyles[i];
+                }
+            }
+            return null;
+        }
+    }
+}
