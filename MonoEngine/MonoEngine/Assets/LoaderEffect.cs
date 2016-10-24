edb8966a5464da7a2f70a9cda42a04dd58065168
aidandearing <aidan.dearing@live.com>
2016-10-23 21:21:39 -0400

    Added Shader Loading
    Added Shader Loading, and allowed models to change their effect. Next
    up, materials.

--- MonoEngine/MonoEngine/Assets/LoaderEffect.cs
+++ MonoEngine/MonoEngine/Assets/LoaderEffect.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MonoEngine.Game;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace MonoEngine.Assets
 {
-    class LoaderSprite : ResourceManagerLoader
+    class LoaderEffect : ResourceManagerLoader
     {
-        public LoaderSprite(Type type) : base(type) { }
+        public LoaderEffect(Type type) : base(type) { }
 
         public override object LoadAsset(string path, string name, Scene parent)
         {
+            // Load a model from a name at the Assets/Models/ + name.xnb pathwa
             // Needs to try to get the model at that name in the models path & load it
-            Texture2D texture = ContentHelper.Content.Load<Texture2D>(path + name);
-            // add the model into the dictionary
+            EffectWrapper effect = new EffectWrapper(ContentHelper.Content.Load<Effect>("Assets/Shaders/" + name));
 
             if (parent != null)
             {
@@ -28,7 +23,7 @@ namespace MonoEngine.Assets
                 // TODO: Log a warning that unbound assets will not unload when a scene switch occurs
             }
 
-            return new Sprite(texture, texture.Bounds, texture.Bounds, Color.White, 0.0f, 1.0f, SpriteEffects.None);
+            return effect;
         }
     }
 }
